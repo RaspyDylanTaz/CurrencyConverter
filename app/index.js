1e0790cfa803f449cf008386830d890b8096b711
Dylan <raspydylan.taz@hotmail.com>
2019-06-07 19:53:11 +1200

    finished lesson 5 of module 3

--- app/index.js
+++ app/index.js
@@ -5,8 +5,10 @@ import EStyleSheet from 'react-native-extended-stylesheet'
 
 EStyleSheet.build({
     $primaryBlue: '#4F6D7A',
-
-    $white: '#fff',
+    $border: '#E2E2E2',
+    $white: '#FFFFFF',
+    $inputText: '#797979',
+    $lightGray: '#f0f0f0',
 });
 
 export default () => <Home />;
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, StatusBar } from 'react-native';
 import { Container } from "../components/Container"
 import { Logo } from '../components/Logo'
+import { InputWithButton } from '../components/TextInput'
 
-export default () => (
 
-    <Container>
-        <StatusBar translucent={false} barStyle="light-content"></StatusBar>
-        <Logo />
-    </Container>
-    )
+const TEMP_BASE_CURRENCY = "USD";
+const TEMP_QUOTE_CURRENCY = 'GBP';
+const TEMP_BASE_PRICE = '100';
+const TEMP_QUOTE_PRICE = '79.74';
+
+
+class Home extends Component {
+
+    handlePressBaseCurrency = () =>{
+        console.log("base press");
+    }
+
+    handlePressQuoteCurrency = () =>{
+        console.log("quote press");
+    }
+
+    render() {
+        return(
+            <Container>
+            <StatusBar translucent={false} barStyle="light-content"></StatusBar>
+            <Logo />
+            <InputWithButton
+                onPress = {this.handlePressTempCurrency}
+                buttonText = {TEMP_BASE_CURRENCY}
+                defaultValue = {TEMP_BASE_PRICE}
+                keyboardType="numeric"
+            />
+            <InputWithButton
+                onPress = {this.handlePressQuoteCurrency}
+                buttonText = {TEMP_QUOTE_CURRENCY}
+                editable={false}
+                value={TEMP_QUOTE_PRICE}
+            />
+
+        </Container>
+        )
+
+    }
+}
+
+export default Home;
